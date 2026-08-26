4de56969ae79e671dc25d11053f2238d48ccc694
Anastasiia Korno <72229275+korno-no@users.noreply.github.com>
2026-08-26 12:59:15 +0000

    fix semantic css

--- src/components/Transaction/Transaction.tsx
+++ src/components/Transaction/Transaction.tsx
@@ -1,8 +1,8 @@
 
 import style from "./Transaction.module.css"
 export default function Transaction({ amount, title, type }: { amount: number, title: string, type: "income" | "expense" }) {
-  return <p className={style[type] + " " + style.transaction}>
+  return <div className={style[type] + " " + style.transaction}>
     <div>{title}</div>
     <div>{type === 'income' ? '+':'-'} {amount}</div>
-  </p>;
+  </div>;
 }
